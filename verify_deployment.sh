ab12ef49aeb25804bc404bc738f63e9ab9628d03
Chintan Shah <kathi.shah@gmail.com>
2024-12-27 14:48:03 -0800

    Refactoring to verify deployment on localhost by default

--- verify_deployment.sh
+++ verify_deployment.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# Set the API URL
-export API_URL="https://audio-analyzer-api-af6843ebf910.herokuapp.com"
+# Default API URL
+API_URL="http://localhost:8000"
+
+# Parse arguments
+for arg in "$@"; do
+    case $arg in
+        --env=prod)
+            API_URL="https://audio-analyzer-api-af6843ebf910.herokuapp.com"
+            shift # Remove --env=prod from arguments
+            ;;
+    esac
+done
+
+# Export the API URL
+export API_URL
 
 # Run deployment tests
-echo "Running deployment verification tests..."
-pytest tests/test_deployment.py -v
+echo "Running deployment verification tests against $API_URL..."
+pytest --api-url=$API_URL tests/test_deployment.py -vvvvv
 
 # Check the exit status
 if [ $? -eq 0 ]; then
